e0e3cc9cb2dcda88add059ad06a634430744ce4b
Nick Porter <e@nck.me.uk>
2016-06-17 23:34:24 +0100

    initial commit

---
+++ ngBackgroundPreload.js
@@ -0,0 +1,38 @@
+/**
+ * ng-background-preload v0.1
+ * AngularJS directive for preloading background images
+ * https://github.com/nckblu
+ */
+
+.directive('ngBackgroundPreload', [
+    function() {
+        return {
+            restrict: 'A',
+            scope: {
+                imageUrl: '@',
+                classOnLoad: '@'
+            },
+            link: function(scope, element) {
+
+                var img = new Image();
+                img.src = scope.imageUrl;
+
+                if (img.complete) {
+                    imageLoaded();
+                    img.onload = function() {};
+                } else {
+                    img.onload = function() {
+                        loaded();
+                        img.onload = function() {};
+                    }
+                }
+
+                function loaded() {
+                    element.css('background-image', 'url("' + scope.imageUrl + '")').addClass(scope.classOnLoad);
+                }
+
+
+            }
+        };
+    }
+])
